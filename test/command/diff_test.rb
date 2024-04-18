6d05fee3ea08915ba1d65375ccb18e6583a4b030
Garrett Bodley <garrett.bodley@gmail.com>
2024-04-18 15:32:25 -0400

    Write comprehensive diff tests.
    Copied over James' tests to ensure the validity of the diff
    implementation.

--- test/command/diff_test.rb
+++ test/command/diff_test.rb
@@ -34,6 +34,9 @@ describe Command::Diff do
         index 12f00e9..5ea2ed4 100644
         --- a/file.txt
         +++ b/file.txt
+        @@ -1,1 +1,1 @@
+        -contents
+        +changed
       DIFF
     end
 
@@ -61,6 +64,9 @@ describe Command::Diff do
         index 12f00e9..5ea2ed4
         --- a/file.txt
         +++ b/file.txt
+        @@ -1,1 +1,1 @@
+        -contents
+        +changed
       DIFF
     end
 
@@ -73,6 +79,8 @@ describe Command::Diff do
         index 12f00e9..0000000
         --- a/file.txt
         +++ /dev/null
+        @@ -1,1 +0,0 @@
+        -contents
       DIFF
     end
   end
@@ -97,6 +105,9 @@ describe Command::Diff do
         index 12f00e9..5ea2ed4 100644
         --- a/file.txt
         +++ b/file.txt
+        @@ -1,1 +1,1 @@
+        -contents
+        +changed
       DIFF
     end
 
@@ -126,6 +137,42 @@ describe Command::Diff do
         index 12f00e9..5ea2ed4
         --- a/file.txt
         +++ b/file.txt
+        @@ -1,1 +1,1 @@
+        -contents
+        +changed
+      DIFF
+    end
+
+    it 'diffs a deleted file' do
+      delete 'file.txt'
+      delete '.git/index'
+      jit_cmd 'add', '.'
+
+      assert_diff_cached <<~DIFF
+        diff --git a/file.txt b/file.txt
+        deleted file mode 100644
+        index 12f00e9..0000000
+        --- a/file.txt
+        +++ /dev/null
+        @@ -1,1 +0,0 @@
+        -contents
+      DIFF
+    end
+
+    it 'diffs an added file' do
+      write_file 'another.txt', <<~FILE
+        hello
+      FILE
+      jit_cmd 'add', '.'
+
+      assert_diff_cached <<~DIFF
+        diff --git a/another.txt b/another.txt
+        new file mode 100644
+        index 0000000..ce01362
+        --- /dev/null
+        +++ b/another.txt
+        @@ -0,0 +1,1 @@
+        +hello
       DIFF
     end
   end
